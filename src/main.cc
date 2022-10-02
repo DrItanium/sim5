fc7b8d1eed8b36a2906682c69c815a9ae21d7dcf
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-10-02 14:54:34 -0700

    Define all the fault codes we can

--- src/main.cc
+++ src/main.cc
@@ -41,7 +41,30 @@ constexpr size_t ConfigurationAddress = 0x7F00;
 constexpr Ordinal SALIGN = 4;
 constexpr Ordinal C = (SALIGN * 16) - 1;
 constexpr Ordinal NotC = ~C;
+/// faults
 constexpr Ordinal NoFault = 0xFFFF'FFFF;
+constexpr Ordinal ParallelFault = 0;
+constexpr Ordinal TraceFaultBase = 0x00010000;
+constexpr Ordinal InstructionTraceFault = TraceFaultBase | 0b00000010;
+constexpr Ordinal BranchTraceFault = TraceFaultBase      | 0b00000100;
+constexpr Ordinal CallTraceFault = TraceFaultBase        | 0b00001000;
+constexpr Ordinal ReturnTraceFault = TraceFaultBase      | 0b00010000;
+constexpr Ordinal PrereturnTraceFault = TraceFaultBase   | 0b00100000;
+constexpr Ordinal SupervisorTraceFault = TraceFaultBase  | 0b01000000;
+constexpr Ordinal MarkTraceFault = TraceFaultBase        | 0b10000000;
+constexpr Ordinal InvalidOpcodeFault = 0x00020001;
+constexpr Ordinal UnimplementedFault = 0x00020002;
+constexpr Ordinal UnalignedFault = 0x00020003;
+constexpr Ordinal InvalidOperandFault = 0x00020004;
+constexpr Ordinal ArithmeticOverflowFault = 0x0003'0001;
+constexpr Ordinal ZeroDivideFault = 0x0003'0002;
+constexpr Ordinal ConstraintRangeFault = 0x0005'0001;
+constexpr Ordinal ProtectionLengthFault = 0x0007'0002;
+constexpr Ordinal ProtectionBadAccessFault = 0x0007'0020;
+
+constexpr Ordinal Machine_ParityErrorFault = 0x0008'0002;
+constexpr Ordinal Type_Mismatch = 0x000a'0001;
+constexpr Ordinal OverrideFault = 0x0010'0000;
 template<typename T>
 volatile T& memory(size_t address) noexcept {
     return *reinterpret_cast<volatile T*>(address);
