98bb291d166782212964b0d12af443fd2d1f273a
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-02-14 15:12:29 -0800

    Use a lookup table instead of doing a direct bit shift computation

--- include/BinaryOperations.h
+++ include/BinaryOperations.h
@@ -31,7 +31,41 @@
 }
 
 [[nodiscard]] constexpr Ordinal computeBitPosition(Ordinal value) noexcept {
-    return static_cast<Ordinal>(1u) << static_cast<Ordinal>(value);
+    constexpr Ordinal positionTable[32] {
+        static_cast<Ordinal>(1ul << 0ul),
+        static_cast<Ordinal>(1ul << 1ul),
+        static_cast<Ordinal>(1ul << 2ul),
+        static_cast<Ordinal>(1ul << 3ul),
+        static_cast<Ordinal>(1ul << 4ul),
+        static_cast<Ordinal>(1ul << 5ul),
+        static_cast<Ordinal>(1ul << 6ul),
+        static_cast<Ordinal>(1ul << 7ul),
+        static_cast<Ordinal>(1ul << 8ul),
+        static_cast<Ordinal>(1ul << 9ul),
+        static_cast<Ordinal>(1ul << 10ul),
+        static_cast<Ordinal>(1ul << 11ul),
+        static_cast<Ordinal>(1ul << 12ul),
+        static_cast<Ordinal>(1ul << 13ul),
+        static_cast<Ordinal>(1ul << 14ul),
+        static_cast<Ordinal>(1ul << 15ul),
+        static_cast<Ordinal>(1ul << 16ul),
+        static_cast<Ordinal>(1ul << 17ul),
+        static_cast<Ordinal>(1ul << 18ul),
+        static_cast<Ordinal>(1ul << 19ul),
+        static_cast<Ordinal>(1ul << 20ul),
+        static_cast<Ordinal>(1ul << 21ul),
+        static_cast<Ordinal>(1ul << 22ul),
+        static_cast<Ordinal>(1ul << 23ul),
+        static_cast<Ordinal>(1ul << 24ul),
+        static_cast<Ordinal>(1ul << 25ul),
+        static_cast<Ordinal>(1ul << 26ul),
+        static_cast<Ordinal>(1ul << 27ul),
+        static_cast<Ordinal>(1ul << 28ul),
+        static_cast<Ordinal>(1ul << 29ul),
+        static_cast<Ordinal>(1ul << 30ul),
+        static_cast<Ordinal>(1ul << 31ul),
+    };
+    return positionTable[static_cast<uint8_t>(value) & 0b11111];
 }
 
 [[nodiscard]] constexpr Ordinal modify(Ordinal mask, Ordinal src, Ordinal srcDest) noexcept {
