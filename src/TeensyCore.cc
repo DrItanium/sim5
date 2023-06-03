e019644f784fdeb070526e1a6534a2d3bef49829
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-06-02 17:41:38 -0700

    Added morse code handler implementation for Teensy as well

--- src/TeensyCore.cc
+++ src/TeensyCore.cc
@@ -24,17 +24,21 @@
 #include "Core.h"
 #ifdef CORE_TEENSY
 #ifdef ARDUINO_TEENSY41
+#include <string>
+constexpr auto LOCKPIN = 33;
+constexpr auto INTPIN = 34;
+constexpr auto BUSYPIN = 35;
+
+constexpr auto FAILPIN = 36;
+constexpr auto LEDPin = LED_BUILTIN;
 void 
 Core::nonPortableBegin() noexcept {
-    //pinMode(LOCKPIN, OUTPUT);
-    //pinMode(FAILPIN, OUTPUT);
-    //pinMode(INTPIN, INPUT);
-    //pinMode(BUSYPIN, INPUT);
-    //pinMode(Address15, OUTPUT); // A15
-    //// Ports K and F are address lines
-    //DDRF = 0xFF; // A23:16
-    //DDRK = 0xFF; // A31:24
-    //// setup the external bus interface and other features too!
+    pinMode(LOCKPIN, OUTPUT);
+    pinMode(FAILPIN, OUTPUT);
+    pinMode(INTPIN, INPUT);
+    pinMode(BUSYPIN, INPUT);
+    pinMode(LEDPin, OUTPUT);
+    digitalWrite(LEDPin, LOW);
 }
 
 
@@ -123,6 +127,201 @@ Core::store(Address address, ByteOrdinal value, TreatAsByteOrdinal) noexcept {
 
 void
 Core::store(Address address, ByteInteger value, TreatAsByteInteger) noexcept {
+}
+namespace morse {
+    constexpr auto UnitDuration = 100;
+    constexpr auto DotDuration = UnitDuration;
+    constexpr auto DashDuration = UnitDuration * 3;
+    constexpr auto LetterDuration = UnitDuration * 3;
+    constexpr auto WordDuration = UnitDuration * 7;
+    void 
+    dot() noexcept {
+        digitalWrite(LEDPin, HIGH);
+        delay(DotDuration);
+        digitalWrite(LEDPin, LOW);
+        delay(UnitDuration);
+    }
+    void
+    dash() noexcept {
+        digitalWrite(LEDPin, HIGH);
+        delay(DashDuration);
+        digitalWrite(LEDPin, LOW);
+        delay(UnitDuration);
+    }
+    void parse (const std::string& str) noexcept;
+    void letter( const std::string& sequence) noexcept;
+    void parse(const std::string& str) noexcept {
+        for (auto c : str) {
+            switch (c) {
+                case '.':
+                    dot();
+                    break;
+                case '-':
+                    dash();
+                    break;
+                case ' ': // word end
+                    delay(WordDuration);
+                    break;
+                case 'A':
+                case 'a':
+                    letter(".-");
+                    break;
+                case 'B':
+                case 'b':
+                    letter("-...");
+                    break;
+                case 'C':
+                case 'c':
+                    letter("-.-.");
+                    break;
+                case 'D':
+                case 'd':
+                    letter("-..");
+                    break;
+                case 'E':
+                case 'e':
+                    letter(".");
+                    break;
+                case 'F':
+                case 'f':
+                    letter("..-.");
+                    break;
+                case 'G':
+                case 'g':
+                    letter("--.");
+                    break;
+                case 'H':
+                case 'h':
+                    letter("....");
+                    break;
+                case 'I':
+                case 'i':
+                    letter("..");
+                    break;
+                case 'J':
+                case 'j':
+                    letter(".---");
+                    break;
+                case 'K':
+                case 'k':
+                    letter("-.-");
+                    break;
+                case 'L':
+                case 'l':
+                    letter(".-..");
+                    break;
+                case 'M':
+                case 'm':
+                    letter("--");
+                    break;
+                case 'N':
+                case 'n':
+                    letter("-.");
+                    break;
+                case 'O':
+                case 'o':
+                    letter("---");
+                    break;
+                case 'P':
+                case 'p':
+                    letter(".--.");
+                    break;
+                case 'Q':
+                case 'q':
+                    letter("--.-");
+                    break;
+                case 'R':
+                case 'r':
+                    letter(".-.");
+                    break;
+                case 'S':
+                case 's':
+                    letter("...");
+                    break;
+                case 't':
+                case 'T':
+                    letter("-");
+                    break;
+                case 'u':
+                case 'U':
+                    letter("..-");
+                    break;
+                case 'V':
+                case 'v':
+                    letter("...-");
+                    break;
+                case 'W':
+                case 'w':
+                    letter(".--");
+                    break;
+                case 'X':
+                case 'x':
+                    letter("-..-");
+                    break;
+                case 'Y':
+                case 'y':
+                    letter("-.--");
+                    break;
+                case 'Z':
+                case 'z':
+                    letter("--..");
+                    break;
+                case '1':
+                    letter(".----");
+                    break;
+                case '2':
+                    letter("..---");
+                    break;
+                case '3':
+                    letter("...--");
+                    break;
+                case '4':
+                    letter("....-");
+                    break;
+                case '5':
+                    letter(".....");
+                    break;
+                case '6':
+                    letter("-....");
+                    break;
+                case '7':
+                    letter("--...");
+                    break;
+                case '8':
+                    letter("---..");
+                    break;
+                case '9':
+                    letter("----.");
+                    break;
+                case '0':
+                    letter("-----");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    void 
+    letter(const std::string& message) noexcept {
+        parse(message);
+        delay(LetterDuration);
+    }
+}
+void 
+Core::checksumFail() noexcept {
+    while(true) {
+        // dot dot dot (S)
+        morse::parse("checksum failure");
+        delay(1000);
+    }
+}
+void 
+Core::selfTestFailure() noexcept {
+    while(true) {
+        // dot dot dot (S)
+        morse::parse("self test failure");
+        delay(1000);
+    }
 }
 #endif /* defined ARDUINO_TEENSY41 */
 #endif /* defined TEENSY_CORE */
