4c63736a914f5c5bc23f753927a2cadf21b650ad
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-07-01 17:09:31 -0700

    isolate mmu_test from i960 design

--- cmd/mmu_test/main.cc
+++ cmd/mmu_test/main.cc
@@ -24,12 +24,74 @@
 // The main for the arduino version of the simulator's main
 // Created by jwscoggins on 8/21/21.
 //
-#include "Core.h"
 #include <iostream>
-#include <filesystem>
-#include <fstream>
 #include <random>
 #include <chrono>
+using Address = uint32_t;
+struct SegmentDescriptor {
+    uint32_t unused[2];
+    union {
+        uint32_t full;
+        struct {
+            uint32_t offset : 6;
+            uint32_t baseAddress : 26;
+        } generic;
+
+    } address;
+    union {
+        uint32_t full;
+        struct {
+            uint32_t valid : 1;
+            uint32_t pagingMethod : 2;
+            uint32_t accessStatus : 5;
+            uint32_t unused0 : 10;
+            uint32_t size : 6;
+            uint32_t unused1 : 4;
+            uint32_t type : 4;
+        } generic;
+    } info;
+};
+static_assert(sizeof(SegmentDescriptor) == (sizeof(uint32_t) * 4));
+struct MMUAddress {
+    explicit constexpr MMUAddress(Address value) : full(value) { }
+    constexpr auto getFullAddress() const noexcept { return full; }
+    constexpr auto getPageOffset() const noexcept { return offset; }
+    constexpr auto getRegion() const noexcept { return region; }
+    constexpr auto getPageTableOffset() const noexcept { return pageTableOffset; }
+    constexpr auto getPageTableDirectoryOffset() const noexcept { return pageTableDirectoryOffset; }
+    constexpr auto isSimpleRegion() const noexcept { return simpleRegionView.unused == 0; }
+    constexpr auto isValidPagedRegion(Address size) const noexcept {
+        return pagedRegionView.pageTableOffset < size;
+    }
+    constexpr auto isValidBipagedRegion(Address size) const noexcept {
+        return pageTableDirectoryOffset < size;
+    }
+    constexpr auto isPagedRegion() const noexcept { return pagedRegionView.unused == 0; }
+    union {
+        Address full;
+        struct {
+            Address offset: 12;
+            Address pageTableOffset: 10;
+            Address pageTableDirectoryOffset: 8;
+            Address region: 2;
+        };
+        struct {
+            Address offset : 12;
+            Address unused : 18;
+            Address region : 2;
+        } simpleRegionView;
+        struct {
+            Address offset : 12;
+            Address pageTableOffset : 10;
+            Address unused : 8;
+            Address region : 2;
+        } pagedRegionView;
+        struct {
+            Address offset : 12;
+            Address baseAddress : 20;
+        } virtualAddress;
+    };
+};
 void
 testOutMMUAddressTranslation() {
     std::cout << "Generating 255 random addresses and determining information based off the address alone" << std::endl;
