595f2ee42bf6410570dd3e81853d46d101f7bf04
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-04-15 15:08:40 -0700

    Remove debugging from call expressions

--- src/CallOperations.cc
+++ src/CallOperations.cc
@@ -25,11 +25,6 @@
 
 void
 Core::enterCall(Ordinal fp) {
-    DEBUG_ENTER_FUNCTION;
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": FP is now 0x" << std::hex << fp << std::endl;
-        std::cout << __PRETTY_FUNCTION__ << ": Frame Index: 0x" << std::hex << static_cast<int>(localRegisterFrameIndex_) << std::endl;
-    }
     // make sure that we synchronize the frame pointer at the time of entering the call
     // this makes sure that the key we tie the set to is properly synchronized with system state
     // if you don't do this, then the old key will be used to save registers
@@ -37,29 +32,11 @@ Core::enterCall(Ordinal fp) {
     getNextPack().takeOwnership(fp, [this](const RegisterFrame& frame, Address address) { saveRegisterFrame(frame, address); });
     ++localRegisterFrameIndex_;
     localRegisterFrameIndex_ %= NumberOfLocalRegisterFrames;
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": Frame Index: 0x" << std::hex << static_cast<int>(localRegisterFrameIndex_) << std::endl;
-    }
-    DEBUG_LEAVE_FUNCTION;
-
 }
 void
 Core::leaveCall() {
-    DEBUG_ENTER_FUNCTION;
     // perform the transfer and modification
-    DEBUG_LOG_LEVEL(2) {
-        auto fp = getFramePointerAddress();
-        std::cout << __PRETTY_FUNCTION__ << ": FP is now 0x" << std::hex << fp << std::endl;
-        std::cout << __PRETTY_FUNCTION__ << ": transferring PFP back to FP" << std::endl;
-    }
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": Frame Index: 0x" << std::hex << static_cast<int>(localRegisterFrameIndex_) << std::endl;
-    }
     moveGPR<Ordinal>(FPIndex, PFPIndex, [](Ordinal input) -> Ordinal { return Register{input}.getPFPAddress(); }, TreatAsOrdinal{});
-    DEBUG_LOG_LEVEL(2) {
-        auto fp = getFramePointerAddress();
-        std::cout << __PRETTY_FUNCTION__ << ": FP is now 0x" << std::hex << fp << std::endl;
-    }
     auto saveRegistersToStack = [this](const RegisterFrame& frame, Address targetAddress) {
         saveRegisterFrame(frame, targetAddress);
     };
@@ -72,10 +49,6 @@ Core::leaveCall() {
                                        });
     --localRegisterFrameIndex_;
     localRegisterFrameIndex_ %= NumberOfLocalRegisterFrames;
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": Frame Index: 0x" << std::hex << static_cast<int>(localRegisterFrameIndex_) << std::endl;
-    }
-    DEBUG_LEAVE_FUNCTION;
 }
 
 void
@@ -88,90 +61,25 @@ Core::setupNewFrameInternals(Ordinal fp, Ordinal temp) noexcept {
 
 OptionalFaultRecord
 Core::callx(Address effectiveAddress) {
-    DEBUG_ENTER_FUNCTION;
     // wait for any uncompleted instructions to finish
     auto temp = getNextFrameBase(); // round stack pointer to next boundary
     auto fp = getFramePointerAddress();
-    DEBUG_LOG_LEVEL(1) {
-        auto g0 = getGPRValue<Ordinal>(GlobalRegisterBase + 0);
-        auto g1 = getGPRValue<Ordinal>(GlobalRegisterBase + 1);
-        auto g2 = getGPRValue<Ordinal>(GlobalRegisterBase + 2);
-        auto g3 = getGPRValue<Ordinal>(GlobalRegisterBase + 3);
-        auto g4 = getGPRValue<Ordinal>(GlobalRegisterBase + 4);
-        auto g5 = getGPRValue<Ordinal>(GlobalRegisterBase + 5);
-        auto g6 = getGPRValue<Ordinal>(GlobalRegisterBase + 6);
-        auto g7 = getGPRValue<Ordinal>(GlobalRegisterBase + 7);
-        std::cout << __PRETTY_FUNCTION__ << ": (0x" << g0 <<
-                  ", 0x" << g1 <<
-                  ", 0x" << g2 <<
-                  ", 0x" << g3 <<
-                  ", 0x" << g4 <<
-                  ", 0x" << g5 <<
-                  ", 0x" << g6 <<
-                  ", 0x" << g7 <<
-                  ")" << std::endl;
-
-        std::cout << __PRETTY_FUNCTION__ << ": (ea): 0x" << std::hex << effectiveAddress << std::endl;
-    }
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": (fp): 0x" << std::hex << fp << std::endl;
-        std::cout << __PRETTY_FUNCTION__ << ": (newFP): 0x" << std::hex << temp << std::endl;
-        auto rip = getGPRValue<Ordinal>(RIPIndex);
-        std::cout << __PRETTY_FUNCTION__ << ": (rip before): 0x" << std::hex << rip << std::endl;
-    }
     balx(RIPIndex, effectiveAddress);
-    DEBUG_LOG_LEVEL(2) {
-        auto rip = getGPRValue<Ordinal>(RIPIndex);
-        std::cout << __PRETTY_FUNCTION__ << ": (rip after): 0x" << std::hex << rip << std::endl;
-    }
     enterCall(fp);
     setupNewFrameInternals(fp, temp);
-    DEBUG_LEAVE_FUNCTION;
     return std::nullopt;
 }
 
 
 OptionalFaultRecord
 Core::call(Integer displacement) {
-    DEBUG_ENTER_FUNCTION;
     // wait for any uncompleted instructions to finish
     auto temp = getNextFrameBase(); // round stack pointer to next boundary
     auto fp = getFramePointerAddress();
-    DEBUG_LOG_LEVEL(1) {
-        auto g0 = getGPRValue<Ordinal>(GlobalRegisterBase + 0);
-        auto g1 = getGPRValue<Ordinal>(GlobalRegisterBase + 1);
-        auto g2 = getGPRValue<Ordinal>(GlobalRegisterBase + 2);
-        auto g3 = getGPRValue<Ordinal>(GlobalRegisterBase + 3);
-        auto g4 = getGPRValue<Ordinal>(GlobalRegisterBase + 4);
-        auto g5 = getGPRValue<Ordinal>(GlobalRegisterBase + 5);
-        auto g6 = getGPRValue<Ordinal>(GlobalRegisterBase + 6);
-        auto g7 = getGPRValue<Ordinal>(GlobalRegisterBase + 7);
-        std::cout << __PRETTY_FUNCTION__ << ": (0x" << g0 <<
-                  ", 0x" << g1 <<
-                  ", 0x" << g2 <<
-                  ", 0x" << g3 <<
-                  ", 0x" << g4 <<
-                  ", 0x" << g5 <<
-                  ", 0x" << g6 <<
-                  ", 0x" << g7 <<
-                  ")" << std::endl;
-    }
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": (fp): 0x" << std::hex << fp << std::endl;
-        std::cout << __PRETTY_FUNCTION__ << ": (disp): 0x" << std::hex << displacement << std::endl;
-        std::cout << __PRETTY_FUNCTION__ << ": (newFP): 0x" << std::hex << temp << std::endl;
-        auto rip = getGPRValue<Ordinal>(RIPIndex);
-        std::cout << __PRETTY_FUNCTION__ << ": (rip before): 0x" << std::hex << rip << std::endl;
-    }
     saveReturnAddress(RIPIndex);
-    DEBUG_LOG_LEVEL(2) {
-        auto rip = getGPRValue<Ordinal>(RIPIndex);
-        std::cout << __PRETTY_FUNCTION__ << ": (rip after): 0x" << std::hex << rip << std::endl;
-    }
     enterCall(fp);
     setupNewFrameInternals(fp, temp);
     branch(displacement);
-    DEBUG_LEAVE_FUNCTION;
     return std::nullopt;
 }
 
@@ -206,39 +114,12 @@ Core::calls(Ordinal src1) {
         // make a copy of the frame pointer and then modify it
         enterCall(fp);
         setupNewFrameInternals(copy.getValue<Ordinal>(), temp);
-        DEBUG_LOG_LEVEL(1) {
-            auto g0 = getGPRValue<Ordinal>(GlobalRegisterBase + 0);
-            auto g1 = getGPRValue<Ordinal>(GlobalRegisterBase + 1);
-            auto g2 = getGPRValue<Ordinal>(GlobalRegisterBase + 2);
-            auto g3 = getGPRValue<Ordinal>(GlobalRegisterBase + 3);
-            auto g4 = getGPRValue<Ordinal>(GlobalRegisterBase + 4);
-            auto g5 = getGPRValue<Ordinal>(GlobalRegisterBase + 5);
-            auto g6 = getGPRValue<Ordinal>(GlobalRegisterBase + 6);
-            auto g7 = getGPRValue<Ordinal>(GlobalRegisterBase + 7);
-            std::cout << __PRETTY_FUNCTION__ << ": (0x" << g0 <<
-                      ", 0x" << g1 <<
-                      ", 0x" << g2 <<
-                      ", 0x" << g3 <<
-                      ", 0x" << g4 <<
-                      ", 0x" << g5 <<
-                      ", 0x" << g6 <<
-                      ", 0x" << g7 <<
-                      ")" << std::endl;
-        }
     }
     return std::nullopt;
 }
 void
 Core::localReturn() {
-    DEBUG_LOG_LEVEL(2) {
-        auto value = getGPRValue<Ordinal>(RIPIndex);
-        std::cout << __PRETTY_FUNCTION__ << ": (rip before): 0x" << std::hex << value << std::endl;
-    }
     restoreStandardFrame();
-    DEBUG_LOG_LEVEL(2) {
-        auto value = getGPRValue<Ordinal>(RIPIndex);
-        std::cout << __PRETTY_FUNCTION__ << ": (rip after): 0x" << std::hex << value <<  std::endl;
-    }
 }
 Ordinal
 Core::restoreACFromStack(Ordinal fp) {
@@ -282,53 +163,38 @@ Core::interruptReturn() {
 }
 OptionalFaultRecord
 Core::ret() {
-    DEBUG_ENTER_FUNCTION;
     syncf();
     auto& pfp = getGPR(PFPIndex);
     switch (pfp.getReturnType()) {
         case 0b000:
-            DEBUG_LOG_LEVEL(2) std::cout << __PRETTY_FUNCTION__ << ": local return" << std::endl;
             localReturn();
             break;
         case 0b001:
-            DEBUG_LOG_LEVEL(2) std::cout << __PRETTY_FUNCTION__ << ": fault return" << std::endl;
             faultReturn();
             break;
         case 0b010:
-            DEBUG_LOG_LEVEL(2) std::cout << __PRETTY_FUNCTION__ << ": supervisor return" << std::endl;
             supervisorReturn(false);
             break;
         case 0b011:
-            DEBUG_LOG_LEVEL(2) std::cout << __PRETTY_FUNCTION__ << ": supervisor-trace return" << std::endl;
             supervisorReturn(true);
             break;
         case 0b111:
-            DEBUG_LOG_LEVEL(2) std::cout << __PRETTY_FUNCTION__ << ": interrupt return" << std::endl;
             interruptReturn();
             break;
         default:
             // undefined!
             return unimplementedFault();
     }
-    DEBUG_LEAVE_FUNCTION;
     return std::nullopt;
 }
 
 
 void
 Core::restoreStandardFrame() noexcept {
-    DEBUG_ENTER_FUNCTION;
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": calling leaveCall" << std::endl;
-    }
 
     leaveCall();
-    DEBUG_LOG_LEVEL(2) {
-        std::cout << __PRETTY_FUNCTION__ << ": calling restoreRIPToIP" << std::endl;
-    }
     restoreRIPToIP();
     advanceInstruction_ = false;
-    DEBUG_LEAVE_FUNCTION;
 }
 
 void
@@ -383,18 +249,11 @@ Core::balx(Register& linkRegister, Ordinal branchTo) {
 
 void
 Core::bal(Integer displacement) {
-    DEBUG_ENTER_FUNCTION;
     saveReturnAddress(LRIndex);
-    DEBUG_LOG_LEVEL(3) {
-        std::cout << "\t" << __PRETTY_FUNCTION__ << ", lr: 0x" << getGPRValue(LRIndex, TreatAsOrdinal{}) << std::endl;
-    }
     branch(displacement);
-    DEBUG_LEAVE_FUNCTION;
 }
 
 void
 Core::bx(Address effectiveAddress) {
-    DEBUG_ENTER_FUNCTION;
     setIP(effectiveAddress);
-    DEBUG_LEAVE_FUNCTION;
 }
